86611e97bcefd4fee46300a949625d5c4b26160e
esettes <ioanaroxana.st@gmail.com>
2023-07-06 00:10:41 +0100

    update

--- src/ft_putendl_fd.c
+++ src/ft_putendl_fd.c
@@ -24,3 +24,17 @@ void	ft_putendl_fd(char *color, char *s, int fd)
 		write (fd, s++, 1);
 	write (fd, "\n", 1);
 }
+
+/*
+void	ft_putendl_fd(char *color, char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	while (*color)
+		write(fd, color++, 1);
+	while (*s)
+		write (fd, s++, 1);
+	color = "\x1b[0m";
+	write (fd, "\n", 1);
+}
+*/
